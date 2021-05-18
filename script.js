84c96cfcfd0d92d93dd6d62a1fef0b2ee8da6a5d
ryandejaegher <dejaegher.ryan@gmail.com>
2021-05-18 12:38:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-<!-- Add Section IDs for anchor links -->
+// Add Section IDs for anchor links
 (function(){
 	window.addEventListener('DOMContentLoaded', function() {
 	var sections = document.querySelectorAll('section');
     var windowPath = window.location.pathname;
+    // If page is set to homepage then set the anchor to home. i.e. #home-1
     if (windowPath === '/') {
         windowPath = 'home';
     }
